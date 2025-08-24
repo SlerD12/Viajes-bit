3a98d5c08e30fff88a05e81b958e2692f80a31cb
SlerD12 <139291283+SlerD12@users.noreply.github.com>
2025-08-23 21:52:45 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,16 +7,31 @@ const urlsToCache = [
   'icon-512.png'
 ];
 
+// Instalación
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
+// Activación (limpia cachés viejos)
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch: primero caché, luego red
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
